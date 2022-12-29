a1ff5e58c0272e3474262877cf19fe04f2b2eb04
PhongNV <Thieugialee01@gmail.com>
2022-12-29 18:10:02 +0700

    UpLoad

---
+++ javascript.js
@@ -0,0 +1,36 @@
+
+const data = [
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+  {
+    link: './imgs/01ed8eb267ab5045d1eb442f5c402e5fd4953f3d.png',
+    content: 'cooking'
+  },
+]
+
+
+const slide = document.querySelector('.slide');
+const listBox = document.querySelectorAll('.container');
+const btnOnClick = document.querySelector('.btnOnClick');
+const content = document.querySelectorAll('.content > p')
+const picture = document.querySelectorAll('.container > img')
+
+
